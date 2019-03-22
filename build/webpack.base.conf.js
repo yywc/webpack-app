9f617c1a4c7725696e574d75c06b7d3a020d1d01
gyj <gyjyywc@gmail.com>
2019-03-22 17:16:59 +0800

    feature: base 文件注释

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+// 路径处理函数
 const resolve = dir => path.join(__dirname, '..', dir);
 
 module.exports = {
@@ -7,12 +8,17 @@ module.exports = {
     app: './src/main.js'
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json'], // 引入 js vue json 文件时可以不用写后缀名
     alias: {
-      '@': resolve('src'),
+      '@': resolve('src'), // 配置 @ 指向 src
     },
   },
   module: {
+    /**
+    * 我们在 vue 项目中需要 loader 来处理的有
+    * vue 文件、js 文件、css/stylus 文件、图片/字体
+    * 所以 rules 的编写就是如下所示
+    */
     rules: [
       {
         test: /\.(js|vue)$/,
